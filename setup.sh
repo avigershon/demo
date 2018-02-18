e9d50f89209cfc10efc01bf4f6ed784317d43b92
avigershon <avigershon@gmail.com>
2018-02-18 17:10:12 +0200

    wait till tiller is ready

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_
 chmod 700 get_helm.sh;
 ./get_helm.sh;
 
-helm init;
+helm init --wait;
 
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
