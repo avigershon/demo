c9e0cdcbb8251d81a6528a64ef42d47bdd633e92
avigershon <avigershon@gmail.com>
2018-02-18 16:49:20 +0200

    Create cluser admin role on setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+ACCOUNT=$(gcloud info --format='value(config.account)')
+kubectl create clusterrolebinding owner-cluster-admin-binding \
+    --clusterrole cluster-admin \
+    --user $ACCOUNT
+    
 project=${PWD##*/}
 
 kubectl create namespace $project;
