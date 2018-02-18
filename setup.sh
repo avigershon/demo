828e3ce14474a54b5180bb56c25b90bc1dd6faf2
avigershon <avigershon@gmail.com>
2018-02-18 17:07:10 +0200

    create tiller service account

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ chmod 700 get_helm.sh;
 
 helm init;
 
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
 mkdir packages;
 
 cd charts;
