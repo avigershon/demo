41cfdd96d1dd709ce9bf767a2c9f7a834cd4be98
Avi Gershon <avigershon@gmail.com>
2018-02-18 14:26:18 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
-for file in charts/* ; helm install .$file; done
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh;
+chmod 700 get_helm.sh;
+./get_helm.sh;
+
+mkdir packages;
+
+cd charts;
+for d in * ; do
+    echo "packaging $d chart...";
+    helm package $d -d "./../packages";
+done
