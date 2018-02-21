abac2b9a6ced8622c1f8749d6836074812ddd18a
avigershon <avigershon@gmail.com>
2018-02-21 02:05:05 +0200

    split setup to system and charts install

---
+++ system-setup.sh
@@ -0,0 +1,68 @@
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --env=*)
+      env="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
+project=${PWD##*/}
+    
+system_setup () {
+
+    ACCOUNT=$(gcloud info --format='value(config.account)')
+
+    kubectl create clusterrolebinding owner-cluster-admin-binding \
+        --clusterrole cluster-admin \
+        --user $ACCOUNT
+
+    kubectl apply -f rolebinding.yaml -o yaml
+
+    kubectl create namespace $project;
+
+    curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh;
+    chmod 700 get_helm.sh;
+    ./get_helm.sh;
+
+    helm init --wait;
+
+    kubectl create serviceaccount --namespace kube-system tiller
+    kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+}
+
+mkdir packages;
+
+cd charts;
+for d in * ; do
+    mkdir ./../packages/$d;
+    echo "packaging $d chart...";
+    helm package $d -d "./../packages/$d";
+done
+
+cd ../packages;
+for d in * ; do
+    cd $d
+    for chart in * ; do
+    
+        #release=$project-$d
+        release=$d
+        
+        if [ $d = "filebeat" ]; then
+            #helm del --purge $d;
+            echo "helm install $chart --name $d";
+            #helm install $chart --name $release --set namespace=$project;
+        else
+            echo "helm upgrade $release $chart -i --wait --namespace $project";
+            helm upgrade $release $chart -i --wait --namespace $project;
+        fi
+        
+    done
+    cd ../
+done
