399c5262b37e593772cfce43b0dc2f5cf706eca4
avigershon <avigershon@gmail.com>
2018-02-21 14:53:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ setup () {
   #  install_chart $env
   fi
   
-  install_chart $env $recreate
+  install_charts $env $recreate
   
 }
 
@@ -53,11 +53,10 @@ system_setup () {
     kubectl create serviceaccount --namespace kube-system tiller
     kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
     kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
-    
-    install_chart $env true
+
 }
 
-install_chart() {
+install_charts() {
 
   project=${PWD##*/}
   home=$PWD
