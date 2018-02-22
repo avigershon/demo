3f4ab04a138748f133120d8d687127843135afda
avigershon <avigershon@gmail.com>
2018-02-22 10:15:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,8 +44,8 @@ system_setup () {
         --clusterrole cluster-admin \
         --user $ACCOUNT
 
-    kubectl apply -f $home/rolebinding.yaml -o yaml
-    kubectl apply -f $home/pv.yaml -o yaml
+    #kubectl apply -f $home/rolebinding.yaml -o yaml
+    #kubectl apply -f $home/pv.yaml -o yaml
     
     curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh;
     chmod 700 get_helm.sh;
