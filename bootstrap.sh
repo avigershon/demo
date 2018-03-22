bf1843943875eceed75e36b9d28b2984464f104c
avigershon <avigershon@gmail.com>
2018-03-22 19:25:53 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,10 @@
 
 #elastic stack
 #./setup.sh --chart cluster/elasticsearch
-helm install --name elasticsearch incubator/elasticsearch --set rbac.create=true
+helm install --name elasticsearch incubator/elasticsearch --set image.repository=docker.elastic.co/elasticsearch/elasticsearch --set image.tag=6.2.3 --set rbac.create=true
 ./setup.sh --chart cluster/filebeat
 #./setup.sh --chart cluster/kibana
-helm install --name kibana --set ingress.enabled=true,ingress.hosts[0]=kibana.local,ingress.annotations[0]="kubernetes.io/ingress.class: nginx" --set env.ELASTICSEARCH_URL=http://elasticsearch-elasticsearch-client.default.svc.cluster.local:9200 stable/kibana
+helm install --name kibana --set image.repository=docker.elastic.co/kibana/kibana --set image.tag=6.2.3 --set ingress.enabled=true,ingress.hosts[0]=kibana.local,ingress.annotations[0]="kubernetes.io/ingress.class: nginx" --set env.ELASTICSEARCH_URL=http://elasticsearch-elasticsearch-client.default.svc.cluster.local:9200 stable/kibana
 
 #kafka
 ./setup.sh --chart cluster/zookeeper
