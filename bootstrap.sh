e5adbf86c5afdfc44708a05161f3dc6d63c6e665
avigershon <avigershon@gmail.com>
2018-03-22 15:08:12 +0200

    added more charts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 ./setup.sh
-./setup.sh --chart cluster
-./setup.sh --chart charts
+
+./setup.sh --chart cluster/cluster
+./setup.sh --chart cluster/nginx-ingress
+
+#elastic stack
+./setup.sh --chart cluster/elasticsearch
+./setup.sh --chart cluster/filebeat
+./setup.sh --chart cluster/kibana
+
+#kafka
+./setup.sh --chart cluster/zookeeper
+./setup.sh --chart cluster/kafka
+
+#node-red
+./setup.sh --chart charts/node-red
