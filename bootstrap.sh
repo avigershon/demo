ece69e5c7e703325329733368ee5cf75cbce9540
avigershon <avigershon@gmail.com>
2018-03-22 19:17:49 +0200

    use the official kibana helm

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,8 @@
 #./setup.sh --chart cluster/elasticsearch
 helm install --name elasticsearch incubator/elasticsearch --set rbac.create=true
 ./setup.sh --chart cluster/filebeat
-./setup.sh --chart cluster/kibana
+#./setup.sh --chart cluster/kibana
+helm install --name kibana --set ingress.enabled=true,ingress.hosts[0]=kibana.local,ingress.annotations[0]="kubernetes.io/ingress.class: nginx" --set env.ELASTICSEARCH_URL=http://elasticsearch-elasticsearch-client.default.svc.cluster.local:9200 stable/kibana
 
 #kafka
 ./setup.sh --chart cluster/zookeeper
