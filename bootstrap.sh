4490badfc42603207258b8515dc5fe07874bc990
avigershon <avigershon@gmail.com>
2018-03-23 18:55:12 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ helm install --name nginx-ingress stable/nginx-ingress
 helm install --name elasticsearch incubator/elasticsearch --set rbac.create=true
 ./setup.sh --chart cluster/filebeat
 #./setup.sh --chart cluster/kibana
-helm upgrade kibana --set image.repository=docker.elastic.co/kibana/kibana --set image.tag=5.4.3 --set ingress.enabled=true --set ingress.hosts[0]=kibana.local --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx --set ingress.annotations."kubernetes\.io/tls-acme"=true --set env.ELASTICSEARCH_URL=http://elasticsearch-elasticsearch-client.default.svc.cluster.local:9200 stable/kibana
+helm upgrade kibana --set image.repository=docker.elastic.co/kibana/kibana --set image.tag=5.4.3 --set service.type=LoadBalancer --set ingress.enabled=true --set ingress.hosts[0]=kibana.local --set ingress.annotations."kubernetes\.io/ingress\.class"=nginx --set ingress.annotations."kubernetes\.io/tls-acme"=true --set env.ELASTICSEARCH_URL=http://elasticsearch-elasticsearch-client.default.svc.cluster.local:9200 stable/kibana
 
 #kafka
 helm install --name kafka incubator/kafka
