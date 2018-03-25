b6d8162037f1d0179c503e0bb42cf7dc5ad90450
avigershon <avigershon@gmail.com>
2018-03-25 22:29:29 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,11 @@
 helm install --name nginx-ingress stable/nginx-ingress
 
 #elastic stack
+rm -r elasticsearch
 git clone https://github.com/clockworksoul/helm-elasticsearch.git elasticsearch
-helm upgrade --name elasticsearch --set image.es.tag=6.2.3 --set kibana.image.tag=6.2.3 --set env.XPACK_GRAPH_ENABLED="true" --set env.XPACK_ML_ENABLED="true" --set env.XPACK_REPORTING_ENABLED="true" --set env.XPACK_SECURITY_ENABLED="true" elasticsearch
+helm install --name elasticsearch --set image.es.tag=6.2.3 --set kibana.image.tag=6.2.3 --set env.XPACK_GRAPH_ENABLED="true" --set env.XPACK_ML_ENABLED="true" --set env.XPACK_REPORTING_ENABLED="true" --set env.XPACK_SECURITY_ENABLED="true" elasticsearch
 
-helm upgrade fluent-bit --set backend.es.host=elasticsearch-elasticsearch stable/fluent-bit
+helm install --name fluent-bit --set backend.es.host=elasticsearch-elasticsearch stable/fluent-bit
 
 #kafka
 helm upgrade --name kafka incubator/kafka
