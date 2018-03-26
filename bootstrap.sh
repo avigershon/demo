d538fe4dd5f64a8a66075bf0e9c995c6215838c3
avigershon <avigershon@gmail.com>
2018-03-26 21:14:20 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ helm install --name elasticsearch --set image.es.tag=6.2.3 --set kibana.image.ta
 #helm install --name fluent-bit --set backend.es.host=elasticsearch-elasticsearch --set rbac.create=true stable/fluent-bit
 
 #kafka
-helm upgrade --name kafka incubator/kafka
+helm upgrade --name kafka --set configurationOverrides."offsets.topic.replication.factor"=5 --set configurationOverrides."auto.offset.commit"=true incubator/kafka
 
 #node-red
 ./setup.sh --chart charts/node-red
