e206de1e37fb9a49754658db61db33516651d4cf
avigershon <avigershon@gmail.com>
2018-03-28 13:04:24 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ helm install --name nginx-ingress stable/nginx-ingress
 rm -r elasticsearch
 git clone https://github.com/clockworksoul/helm-elasticsearch.git elasticsearch
 helm install --name elasticsearch  --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana --set kibana.image.tag=6.2.3 --set kibana.env.XPACK_GRAPH_ENABLED="true" --set kibana.env.XPACK_ML_ENABLED="true" --set kibana.env.XPACK_REPORTING_ENABLED="true" --set kibana.env.XPACK_SECURITY_ENABLED="true" elasticsearch
-#helm upgrade elasticsearch --set image.es.tag=6.2.3 --set image.repository=docker.elastic.co/kibana/kibana --set kibana.image.tag=6.2.3 --set kibana.env.XPACK_GRAPH_ENABLED="true" --set kibana.env.XPACK_ML_ENABLED="true" --set kibana.env.XPACK_REPORTING_ENABLED="true" --set kibana.env.XPACK_SECURITY_ENABLED="true" elasticsearch
+#helm upgrade elasticsearch --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana --set kibana.image.tag=6.2.3 --set kibana.env.XPACK_GRAPH_ENABLED="true" --set kibana.env.XPACK_ML_ENABLED="true" --set kibana.env.XPACK_REPORTING_ENABLED="true" --set kibana.env.XPACK_SECURITY_ENABLED="true" elasticsearch
 
 #helm install --name fluent-bit --set backend.es.host=elasticsearch-elasticsearch --set rbac.create=true stable/fluent-bit
 
