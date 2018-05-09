062242730a1b7ffce50373cc609fb734e2c67042
avigershon <avigershon@gmail.com>
2018-05-09 06:04:03 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,9 @@ git clone https://github.com/clockworksoul/helm-elasticsearch.git elasticsearch
 helm install --name elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme elasticsearch
 #helm upgrade elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme elasticsearch
 
+helm install --name logstash -f cluster/logstash/values.yaml incubator/logstash
+helm upgrade logstash -f cluster/logstash/values.yaml incubator/logstash
+
 #helm del filebeat --purge
 #helm install --name filebeat stable/filebeat  
 ./setup.sh --chart cluster/filebeat
