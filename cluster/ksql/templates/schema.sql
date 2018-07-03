46138cb39a1ca0da78b8133f6bbfff3651cb4815
avigershon <avigershon@gmail.com>
2018-07-03 23:17:16 +0300

    Update schema.sql

--- cluster/ksql/templates/schema.sql
+++ cluster/ksql/templates/schema.sql
@@ -9,5 +9,8 @@ DROP STREAM IF EXISTS sport_events;
 
 CREATE STREAM sport_events(name varchar, source_name varchar,keywords varchar,league_name varchar,sport_name varchar,minute int, date bigint,score varchar, score_1 varchar, score_2 varchar, timestamp bigint, id varchar) with (kafka_topic = 'SPORT_EVENT', value_format = 'json', KEY='id', TIMESTAMP='timestamp');
 
+DROP TABLE IF EXISTS sport_events_unique_per_min_unique;
+CREATE table sport_events_unique_per_min_unique AS SELECT max(minute) minute, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY id;
+
 DROP TABLE IF EXISTS sport_events_per_min;
-CREATE table sport_events_per_min AS SELECT name, source_name, league_name, sport_name, minute, date, score, score_1, score_2, timestamp, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY name, source_name, league_name, sport_name, minute, date, score, score_1, score_2, timestamp, id;
+CREATE table sport_events_per_min AS SELECT name, source_name, league_name, sport_name, sport_events.minute, date, score, score_1, score_2, timestamp, sport_events.id, events FROM sport_events LEFT JOIN sport_events_unique_per_min_unique ON sport_events.id = sport_events_unique_per_min_unique.id and sport_events.minute=sport_events_unique_per_min_unique.minute;
