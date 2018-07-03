d06873be0960c21dc08b02f3234c27cfa9ef68b5
avigershon <avigershon@gmail.com>
2018-07-03 22:17:01 +0300

    Update schema.sql

--- cluster/ksql/templates/schema.sql
+++ cluster/ksql/templates/schema.sql
@@ -10,4 +10,4 @@ DROP STREAM IF EXISTS sport_events;
 CREATE STREAM sport_events(name varchar, source_name varchar,keywords varchar,league_name varchar,sport_name varchar,minute int, date bigint,score varchar, score_1 varchar, score_2 varchar, timestamp bigint, id varchar) with (kafka_topic = 'SPORT_EVENT', value_format = 'json', KEY='id', TIMESTAMP='timestamp');
 
 DROP TABLE IF EXISTS sport_events_per_min;
-CREATE table sport_events_per_min AS SELECT name, source_name, league_name, sport_name, minute, date, score, score_1, score_2, timestamp, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY name, source_name, league_name, sport_name, minut, date, score, score_1, score_2, timestamp, id;
+CREATE table sport_events_per_min AS SELECT name, source_name, league_name, sport_name, minute, date, score, score_1, score_2, timestamp, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY name, source_name, league_name, sport_name, minute, date, score, score_1, score_2, timestamp, id;
