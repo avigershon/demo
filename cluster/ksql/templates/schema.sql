49ed59abb7bb463bcadf19a12fc3c4fd5d388d41
avigershon <avigershon@gmail.com>
2018-07-04 01:11:40 +0300

    Update schema.sql

--- cluster/ksql/templates/schema.sql
+++ cluster/ksql/templates/schema.sql
@@ -13,4 +13,6 @@ DROP TABLE IF EXISTS sport_events_unique_per_min_unique;
 CREATE table sport_events_unique_per_min_unique AS SELECT max(minute) minute, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY id;
 
 DROP TABLE IF EXISTS sport_events_per_min;
-CREATE table sport_events_per_min AS SELECT name, source_name, league_name, sport_name, sport_events.minute, date, score, score_1, score_2, timestamp, sport_events.id, events FROM sport_events LEFT JOIN sport_events_unique_per_min_unique ON sport_events.id = sport_events_unique_per_min_unique.id and sport_events.minute=sport_events_unique_per_min_unique.minute;
+CREATE STREAM sport_events_per_min WITH (PARTITIONS=4) AS SELECT name, source_name, league_name, sport_name, sport_events.minute as minute, date, score, score_1, score_2, timestamp, sport_events.id, events FROM sport_events LEFT JOIN sport_events_unique_per_min_unique ON sport_events.id = sport_events_unique_per_min_unique.id; 
+
+//AND sport_events.minute=sport_events_unique_per_min_unique.minute;
