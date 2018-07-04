564006093b37b91def761e93722f2522c766f3b1
avigershon <avigershon@gmail.com>
2018-07-04 13:44:38 +0300

    Update schema.sql

--- cluster/ksql/templates/schema.sql
+++ cluster/ksql/templates/schema.sql
@@ -12,9 +12,5 @@ CREATE STREAM sport_events(name varchar, source_name varchar,keywords varchar,le
 DROP TABLE IF EXISTS sport_events_unique_per_min_unique;
 CREATE table sport_events_unique_per_min_unique AS SELECT name,max(minute) minute, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY id;
 
-select name,minute,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][0]') as ah_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][1]') as ah_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][2]') as ah_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][0]') as ah_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][1]') as ah_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][2]') as ah_1_run_line from sport_events;
-
-DROP STREAM IF EXISTS sport_events_per_min;
-CREATE STREAM sport_events_per_min WITH (PARTITIONS=4) AS SELECT name, source_name, league_name, sport_name, sport_events.minute as minute, date, score, score_1, score_2, timestamp, sport_events.id AS id, events FROM sport_events LEFT JOIN sport_events_unique_per_min_unique ON sport_events.id = sport_events_unique_per_min_unique.id PARTITION BY sport_events.id; 
-
-//AND sport_events.minute=sport_events_unique_per_min_unique.minute;
+DROP STREAM IF EXISTS 1betpro_events;
+CREATE STREAM onebetpro_events AS SELECT name,source_name,minute,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][0]') as ah_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][1]') as ah_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][2]') as ah_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][0]') as ah_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][1]') as ah_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][2]') as ah_1_run_line ,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][0]') as ou_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][1]') as ou_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][2]') as ou_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][0]') as ou_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][1]') as ou_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][2]') as ou_1_run_line from sport_events;
