e1efcccb12a97195bab4eca180f57772caccf72f
avigershon <avigershon@gmail.com>
2018-07-04 13:49:16 +0300

    Update schema.sql

--- cluster/ksql/templates/schema.sql
+++ cluster/ksql/templates/schema.sql
@@ -9,8 +9,12 @@ DROP STREAM IF EXISTS sport_events;
 
 CREATE STREAM sport_events(name varchar, source_name varchar,keywords varchar,league_name varchar,sport_name varchar,minute int, date bigint,score varchar, score_1 varchar, score_2 varchar, timestamp bigint, id varchar, sg map<VARCHAR, VARCHAR>) with (kafka_topic = 'SPORT_EVENT', value_format = 'json', KEY='id', TIMESTAMP='timestamp');
 
-DROP TABLE IF EXISTS sport_events_unique_per_min_unique;
-CREATE table sport_events_unique_per_min_unique AS SELECT name,max(minute) minute, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY id;
+#DROP TABLE IF EXISTS sport_events_unique_per_min_unique;
+#CREATE table sport_events_unique_per_min_unique AS SELECT name,max(minute) minute, id, count(*) AS events FROM sport_events window TUMBLING (size 60 second) GROUP BY id;
 
-DROP STREAM IF EXISTS 1betpro_events;
+DROP STREAM IF EXISTS onebetpro_events;
 CREATE STREAM onebetpro_events AS SELECT name,source_name,minute,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][0]') as ah_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][1]') as ah_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][2]') as ah_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][0]') as ah_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][1]') as ah_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][2]') as ah_1_run_line ,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][0]') as ou_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][1]') as ou_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][2]') as ou_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][0]') as ou_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][1]') as ou_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][2]') as ou_1_run_line from sport_events;
+
+#DROP STREAM IF EXISTS sport_events_per_min;
+#CREATE STREAM sport_events_per_min AS SELECT name,source_name,minute,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][0]') as ah_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][1]') as ah_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][0][2]') as ah_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][0]') as ah_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][1]') as ah_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ah[0][1][2]') as ah_1_run_line ,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][0]') as ou_0_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][1]') as ou_0_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][0][2]') as ou_0_run_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][0]') as ou_1_id,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][1]') as ou_1_money_line,EXTRACTJSONFIELD(sg['1'],'$.ft.ou[0][1][2]') as ou_1_run_line from sport_events;
+
