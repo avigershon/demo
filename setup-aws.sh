d5d767dca4551254cde02c5f6ca72d7109c72a8e
avigershon <avigershon@gmail.com>
2018-08-09 12:22:07 +0300

    Update setup-aws.sh

--- setup-aws.sh
+++ setup-aws.sh
@@ -241,7 +241,7 @@ package_and_install_chart () {
 
 aws_client_setup () {
 
-   # ./setup-aws.sh --ClusterControlPlaneSecurityGroup sg-20459f57 --ClusterName ashford_4 --KeyName data_team_key --NodeAutoScalingGroupMaxSize3 --NodeAutoScalingGroupMinSize1 --NodeImageId ami-dea4d5a1 --NodeInstanceType t2.medium --Subnets subnet-2f21db59\\,subnet-1a27dd6c\\,subnet-a0d085e8 --VpcId vpc-888730ec
+   # ./setup-aws.sh --ClusterControlPlaneSecurityGroup sg-20459f57 --ClusterName ashford_4 --KeyName data_team_key --NodeAutoScalingGroupMaxSize3 --NodeAutoScalingGroupMinSize1 --NodeImageId ami-dea4d5a1 --NodeInstanceType t2.medium --Subnets subnet-2f21db59,subnet-1a27dd6c,subnet-a0d085e8,subnet-844d51dd,subnet-0f485456 --VpcId vpc-888730ec
 
    cleanClusterName=${ClusterName/_/-}
    stackName=eks-$cleanClusterName-worker-nodes
@@ -272,7 +272,9 @@ aws_client_setup () {
    if [ "$workersStackStatus" != "CREATE_COMPLETE" ]; then
   
       echo "Step 2 -- AWS setup - Creating EKS worker nodes"
-      aws cloudformation create-stack --stack-name $stackName --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/amazon-eks-nodegroup.yaml --parameters ParameterKey=ClusterControlPlaneSecurityGroup,ParameterValue=$ClusterControlPlaneSecurityGroup ParameterKey=ClusterName,ParameterValue=$ClusterName ParameterKey=KeyName,ParameterValue=$KeyName ParameterKey=NodeAutoScalingGroupMaxSize,ParameterValue=$NodeAutoScalingGroupMaxSize ParameterKey=NodeAutoScalingGroupMinSize,ParameterValue=$NodeAutoScalingGroupMinSize ParameterKey=NodeGroupName,ParameterValue=$ClusterName-node-group ParameterKey=NodeImageId,ParameterValue=$NodeImageId ParameterKey=NodeInstanceType,ParameterValue=$NodeInstanceType ParameterKey=VpcId,ParameterValue=$VpcId ParameterKey=Subnets,ParameterValue=$Subnets
+      
+      decodeSubnets=${Subnets/,/\\,}
+      aws cloudformation create-stack --stack-name $stackName --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/amazon-eks-nodegroup.yaml --parameters ParameterKey=ClusterControlPlaneSecurityGroup,ParameterValue=$ClusterControlPlaneSecurityGroup ParameterKey=ClusterName,ParameterValue=$ClusterName ParameterKey=KeyName,ParameterValue=$KeyName ParameterKey=NodeAutoScalingGroupMaxSize,ParameterValue=$NodeAutoScalingGroupMaxSize ParameterKey=NodeAutoScalingGroupMinSize,ParameterValue=$NodeAutoScalingGroupMinSize ParameterKey=NodeGroupName,ParameterValue=$ClusterName-node-group ParameterKey=NodeImageId,ParameterValue=$NodeImageId ParameterKey=NodeInstanceType,ParameterValue=$NodeInstanceType ParameterKey=VpcId,ParameterValue=$VpcId ParameterKey=Subnets,ParameterValue=$decodeSubnets
    
       while [[ "$workersStackStatus" != "CREATE_COMPLETE" ]];do 
           workersStackStatus=$( aws cloudformation describe-stacks --stack-name $stackName --query 'Stacks[*].StackStatus' --output text) 
