368f3dd4e57df3fd8cef306f161f0d2382fd43bc
avigershon <avigershon@gmail.com>
2018-08-12 16:18:58 +0300

    Update bootstrap-aws.sh

--- bootstrap-aws.sh
+++ bootstrap-aws.sh
@@ -23,8 +23,8 @@ helm install --name elasticsearch --set common.stateful.enabled=true --set image
 #helm upgrade elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme --set common.stateful.class=nifi-storage-class elasticsearch
 
 #helm del logstash --purge
-helm install --name logstash -f cluster/logstash/values-ashford.yaml incubator/logstash
-#helm upgrade logstash -f cluster/logstash/values-ashford.yaml incubator/logstash
+helm install --name logstash --set  persistence.storageClass=nifi-storage-class -f cluster/logstash/values-ashford.yaml incubator/logstash
+#helm upgrade logstash --set  persistence.storageClass=nifi-storage-class -f cluster/logstash/values-ashford.yaml incubator/logstash
 
 #kafka
 #helm del kafka --purge
