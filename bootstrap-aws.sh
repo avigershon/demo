a7886943e9152892a7a43718bb94de15d88a9e05
avigershon <avigershon@gmail.com>
2018-08-12 14:10:07 +0300

    Update bootstrap-aws.sh

--- bootstrap-aws.sh
+++ bootstrap-aws.sh
@@ -14,8 +14,8 @@ git clone https://github.com/clockworksoul/helm-elasticsearch.git elasticsearch
 # change cronJob apiVersion from v2alpha1 to v1beta1
 # nano elasticsearch/templates/_helpers.tpl
 #helm del elasticsearch --purge
-helm install --name elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme elasticsearch
-#helm upgrade elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme elasticsearch
+helm install --name elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme --set common.stateful.class=nifi-storage-class elasticsearch
+#helm upgrade elasticsearch --set common.stateful.enabled=true --set image.es.tag=6.2.3 --set kibana.image.repository=docker.elastic.co/kibana/kibana-oss --set kibana.image.tag=6.2.3 --set kibana.env.ELASTICSEARCH_USERNAME=elastic --set kibana.env.ELASTICSEARCH_PASSWORD=changeme --set common.stateful.class=nifi-storage-class elasticsearch
 
 #helm del logstash --purge
 #helm install --name logstash -f cluster/logstash/values.yaml incubator/logstash
